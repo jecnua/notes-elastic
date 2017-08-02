cc61ea8696c7e7afd50f7614a933c6ac2e8a7a41
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-08-02 13:46:41 +0100

    Fixed the README generator

--- run_me.sh
+++ run_me.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-#Slides
 
-#Links
 # cat README_PRE.md > README.md
-echo "# Links" > README.md
-# echo " "
-echo "## Articles" > README.md
-# echo " "
+echo "# Notes, slides and stuff" > README.md
+echo "## Articles" >> README.md
 find . -name *.article | while read line; do
-    # printf "-" >> README.md
-    HH=`echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
+    HH=`echo '- ' && echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
     echo $HH >> README.md
     echo " " >> README.md
 done
 echo "## Slides" >> README.md
 echo " "
 find . -name *.slide | while read line; do
-    HH=`echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo "http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
+    HH=`echo '- ' && echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo "http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
     echo $HH >> README.md
     echo " " >> README.md
 done

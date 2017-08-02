07be934f49718da64a4dee3f3e3f06078f73e302
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-08-02 12:07:24 +0100

    Working on the link generator

--- run_me.sh
+++ run_me.sh
@@ -8,4 +8,5 @@ cat README_PRE.md > README.md
 find . -name *.article | while read line; do
     HH=`echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo "http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
     echo $HH >> README.md
+    echo " " >> README.md
 done

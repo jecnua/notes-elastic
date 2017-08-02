c0f99212d6eaf34596a38033a830aad33c1b008f
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-08-02 12:13:03 +0100

    working on readme 2

--- run_me.sh
+++ run_me.sh
@@ -12,7 +12,7 @@ find . -name *.article | while read line; do
     # printf "-" >> README.md
     HH=`echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
     echo $HH >> README.md
-    # echo " " >> README.md
+    echo " " >> README.md
 done
 echo "## Slides" >> README.md
 echo " "

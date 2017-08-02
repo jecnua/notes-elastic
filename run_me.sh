5bab3c067ed529a77507eba407bfa0e0a14f8927
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-08-02 14:20:18 +0100

    Better README

--- run_me.sh
+++ run_me.sh
@@ -3,16 +3,19 @@
 
 # cat README_PRE.md > README.md
 echo "# Notes, slides and stuff" > README.md
+echo " " >> README.md
 echo "## Articles" >> README.md
+echo " " >> README.md
 find . -name *.article | while read line; do
     HH=`echo '- ' && echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
     echo $HH >> README.md
-    echo " " >> README.md
+    # echo " " >> README.md
 done
+echo " " >> README.md
 echo "## Slides" >> README.md
-echo " "
+echo " " >> README.md
 find . -name *.slide | while read line; do
     HH=`echo '- ' && echo $line | cut -d "." -f 2 -f 3 | xargs -L 1 echo "http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' '`
     echo $HH >> README.md
-    echo " " >> README.md
+    # echo " " >> README.md
 done

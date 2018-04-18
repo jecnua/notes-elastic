9bed3d713b3d70220698f9c31495c34fca0b06a7
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-04-18 18:14:34 +0100

    Fixed all links

--- run_me.sh
+++ run_me.sh
@@ -7,9 +7,9 @@ echo "## Articles"
 echo " "
 } > README.md
 find . -name "*.article" | while read line; do
-    A_LINK=$(echo -n '- ' && echo "$line" | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' ')
-    NAME=$(echo "$A_LINK" | sed "s/http:\/\/go-talks.appspot.com\/github.com\/jecnua\/notes-presentations//")
-    echo -e "[$NAME]($A_LINK) \n" >> README.md
+  A_LINK=$(echo "$line" | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' ')
+  NAME=$(echo "$line" | cut -d "." -f 2 -f 3)
+  echo -e "- [$NAME]($A_LINK)" >> README.md
 done
 {
 echo " "
@@ -17,6 +17,7 @@ echo "## Slides"
 echo " "
 } >> README.md
 find . -name "*.slide" | while read line; do
-    A_LINK=$(echo -n '- ' && echo "$line" | cut -d "." -f 2 -f 3 | xargs -L 1 echo "http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' ')
-    echo "$A_LINK" >> README.md
+  A_LINK=$(echo "$line" | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' ')
+  NAME=$(echo "$line" | cut -d "." -f 2 -f 3)
+  echo -e "- [$NAME]($A_LINK)" >> README.md
 done

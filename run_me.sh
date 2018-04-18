d68fdfa5f1f73571d123a32e41d9ece978fc008b
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2018-04-18 18:15:16 +0100

    Small changes

--- run_me.sh
+++ run_me.sh
@@ -6,7 +6,7 @@ echo " "
 echo "## Articles"
 echo " "
 } > README.md
-find . -name "*.article" | while read line; do
+find . -name "*.article" | while read -r line; do
   A_LINK=$(echo "$line" | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' ')
   NAME=$(echo "$line" | cut -d "." -f 2 -f 3)
   echo -e "- [$NAME]($A_LINK)" >> README.md
@@ -16,7 +16,7 @@ echo " "
 echo "## Slides"
 echo " "
 } >> README.md
-find . -name "*.slide" | while read line; do
+find . -name "*.slide" | while read -r line; do
   A_LINK=$(echo "$line" | cut -d "." -f 2 -f 3 | xargs -L 1 echo " http://go-talks.appspot.com/github.com/jecnua/notes-presentations" | tr -d ' ')
   NAME=$(echo "$line" | cut -d "." -f 2 -f 3)
   echo -e "- [$NAME]($A_LINK)" >> README.md
